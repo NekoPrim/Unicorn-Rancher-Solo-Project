10fd7956c275702e6240c56359ad91c43d9b795a
Desiree Quade <nekodez@gmail.com>
2022-02-10 13:41:02 -0600

    completed selected answer

--- src/components/Answer/AnswerOne.jsx
+++ src/components/Answer/AnswerOne.jsx
@@ -1,24 +1,32 @@
-import React, { useDispatch } from 'react';
+import { useDispatch } from 'react-redux';
 import useReduxStore from '../../hooks/useReduxStore';
 import { useHistory } from 'react-router-dom';
 
 const AnswerOne = () => {
 
+    // setup dispatch and history
+    const dispatch = useDispatch();
+    const history = useHistory();
+
     // access global variables
     const store = useReduxStore();
 
     const handleSelected = (content) => {
-        dispatchEvent({
+        dispatch({
             type: 'SET_SELECTED',
             payload: content
+        });
+        dispatch({
+            type: "USER_ANSWER",
+            payload: content.id
         })
     }
 
     return(
         <div>
             {store.answer.map((content, id) => (
-                <div key={id} onClick={() => handleSelected(content)}>
-                    <h3>{content.content}</h3>
+                <div key={id}>
+                    <h4 onClick={() => handleSelected(content)}>{content.content}</h4>
                 </div>
             ))}
         </div>

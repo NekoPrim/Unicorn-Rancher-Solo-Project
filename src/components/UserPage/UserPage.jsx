090be8cd584550e28b95106258c207f0a60a0fa8
Desiree Quade <nekodez@gmail.com>
2022-02-14 13:45:32 -0600

    forgot to switch back to style branch after pushing

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,11 +3,13 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
 function UserPage() {
+  
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <>
       <div className="container">
+      {/* <img src="../../../public/images/Cartoon-Unicorn.jpeg" /> */}
         <h2>Welcome, {user.username}!</h2>
         <p>Your ID is: {user.id}</p>
         <LogOutButton className="btn" />
